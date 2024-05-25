86bf58fe013a81b03190b80a3cc5426724cc6320
Christian Palazzo <palazzochristian@yahoo.it>
2024-05-25 23:18:42 +0200

    home page template implementation

---
+++ app/ui/fonts.ts
@@ -0,0 +1,26 @@
+import { Ubuntu, Lusitana } from 'next/font/google';
+ 
+export const ubuntu300 = Ubuntu({ 
+    weight: "300",
+    subsets: ["latin"]
+});
+
+export const ubuntu400 = Ubuntu({ 
+    weight: "400",
+    subsets: ["latin"]
+});
+
+export const ubuntu500 = Ubuntu({ 
+    weight: "500",
+    subsets: ["latin"]
+});
+
+export const ubuntu700 = Ubuntu({ 
+    weight: "700",
+    subsets: ["latin"]
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
